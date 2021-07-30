bd63c490df535b825adedddb467728f0cff1ff94
Emerson Lima <68118899+Strubis@users.noreply.github.com>
2021-07-29 23:02:27 -0300

    Adicionando um exemplo com Datas

---
+++ Bootcamp Java/DateUse.java
@@ -0,0 +1,30 @@
+package com.desenvolvimento.basico;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
+/**
+ *
+ * @author Emerson
+ */
+public class DateUse {
+    public static void main(String[] args) {
+        Calendar now = Calendar.getInstance();
+        
+        // Data atual
+        System.out.println( "Data da compra: " + now.getTime() );
+        
+        // Depois de 10 dias
+        now.add( Calendar.DATE, 10 );
+        System.out.println( "Vencimento: " + now.getTime() );
+        
+        
+        // Convertendo uma data para DD/MM/YYYY HH:MM:SS:MMM
+        SimpleDateFormat formatter = new SimpleDateFormat( "dd/MM/yyyy HH:mm:ss:mmm" );
+        Date date = new Date();
+        
+        String formatDate = formatter.format( date );
+        System.out.println( "\nData: " + formatDate );
+    }
+}
